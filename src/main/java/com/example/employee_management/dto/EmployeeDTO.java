6223481f3343e330ede86a2e6960a937f5ae0a0b
manmohanmohan <manmohan.moh@gmail.com>
2024-06-23 22:51:46 +0100

    modified dto class

--- src/main/java/com/example/employee_management/dto/EmployeeDTO.java
+++ src/main/java/com/example/employee_management/dto/EmployeeDTO.java
@@ -2,7 +2,6 @@ package com.example.employee_management.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
